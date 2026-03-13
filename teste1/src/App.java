747938158919e5ff1bfeb5a57608db801b0b5393
Henrique Nunes <h.nunes@edu.pucrs.br>
2026-03-13 00:59:32 +0000

    initial commit

---
+++ teste1/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("\nHello world!");
+        String version = System.getProperty("java.version");
+        System.out.println("Running Java Version: " + version + "\n");
+        String name = System.getProperty("user.name");
+        System.out.println("User account name: " + name + "\n");
+        String workspace = System.getProperty("user.dir");
+        System.out.println("Current working directory: " + workspace + "\n");
+    }
+}
